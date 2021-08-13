0ed46c4b1df2e40ef6912d51850cd47112b1671a
Sergey Krylov <skrylov@pult.ru>
2021-08-13 16:01:56 +0300

    v1.1

--- _src/assets/js/app.js
+++ _src/assets/js/app.js
@@ -1,32 +1,4 @@
 
-// const
-//    banner = document.querySelector('.banner');
-
-
-
-// document.addEventListener('click', (event) => {
-//    if (event.target.classList.contains('description__ball') || event.target.classList.contains('description__text') || event.target.classList.contains('description')) {
-//       dropDescription.forEach((description) => {
-//          description.closest('.section__descriptions').querySelectorAll('.description').forEach((item) => {
-//             item.classList.remove('active');
-//          });
-//          event.target.closest('.description').classList.add('active');
-//          description.closest('.section__descriptions').querySelector('.description__title').classList.add('hidden');
-//       });
-//    } else
-//       if (event.target.matches('.header__menu-burger') || event.target.matches('.header__burger')) {
-//          event.target.closest('.header__menu-burger').classList.toggle('active');
-//          event.target.closest('.header').querySelector('.header__menu').classList.toggle('active');
-//       }
-//       else {
-//          dropDescription.forEach((description) => {
-//             description.closest('.section__descriptions').querySelectorAll('.description').forEach((item) => {
-//                item.classList.remove('active');
-//             });
-//          });
-//          document.querySelector('.header__menu-burger').classList.remove('active');
-//          document.querySelector('.header__menu').classList.remove('active');
-
-//       }
-
-// });
+document.getElementById('menu__burger').onclick = function () {
+   document.getElementById('main').classList.toggle('main-min');
+}
